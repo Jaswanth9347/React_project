093dcb439b7f0fe49bb85dbea75c65853f9dfd61
Jaswanth9347 <thorjaswanth970@gmail.com>
2025-06-19 00:14:19 +0530

    Formatting Document

--- E-commerce/src/Components/Header.jsx
+++ E-commerce/src/Components/Header.jsx
@@ -6,17 +6,17 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 export default function Header() {
-    const cartItems=useSelector((state)=>state.cartItems.cart);
-    const uniqueCount=cartItems.length
+    const cartItems = useSelector((state) => state.cartItems.cart);
+    const uniqueCount = cartItems.length
     return (
         <div className='flex items-center justify-between px-4 md:px-8 py-4 shadow-lg bg-white flex-wrap'>
             <Link to="/">
-            <div className='flex items-center gap-2 cursor-pointer font-bold  text-2xl md:text-4xl'>
-                <IoBagHandle />
-                <h3>Mart</h3>
-            </div>
+                <div className='flex items-center gap-2 cursor-pointer font-bold  text-2xl md:text-4xl'>
+                    <IoBagHandle />
+                    <h3>Mart</h3>
+                </div>
             </Link>
-            <div className='flex flex-wrap items-center cursor-pointer text-xl sm:text-2xl md:text-3xl space-x-4 sm:space-x-6 md:space-x-10 mt-4 md:mt-0'>           
+            <div className='flex flex-wrap items-center cursor-pointer text-xl sm:text-2xl md:text-3xl space-x-4 sm:space-x-6 md:space-x-10 mt-4 md:mt-0'>
                 <Link to="/"><h3>Home</h3></Link>
                 <Link to='/shop'><h3>Shop</h3></Link>
                 <Link to='/cart'><h3>Cart</h3></Link>
