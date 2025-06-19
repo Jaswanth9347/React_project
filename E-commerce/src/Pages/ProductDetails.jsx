02299f01bf68135b938677cfa4aba3af9b450559
Jaswanth9347 <thorjaswanth970@gmail.com>
2025-06-19 10:05:48 +0530

    Adding comments to understand code

--- E-commerce/src/Pages/ProductDetails.jsx
+++ E-commerce/src/Pages/ProductDetails.jsx
@@ -79,6 +79,9 @@ export default function ProductDetails() {
               onChange={(e) => setQuantity(Number(e.target.value))}
               className="w-20 h-10 border border-gray-400 rounded text-center mb-5"
             /><br />
+
+            {/* On click EVent Handling */}
+            
             <button
               className="bg-blue-900 px-6 py-2 h-12 w-40 text-white rounded hover:bg-blue-800"
               onClick={handleAddToCart}
